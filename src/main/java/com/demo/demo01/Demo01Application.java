d8eea449de1315eef682c1a30e89fbb9de76a393
wangfengg <441685975@qq.com>
2018-08-15 09:49:23 +0800

    rrr

--- src/main/java/com/demo/demo01/Demo01Application.java
+++ src/main/java/com/demo/demo01/Demo01Application.java
@@ -1,11 +1,13 @@
 package com.demo.demo01;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@MapperScan("com.demo.demo01.mapper")
 public class Demo01Application {
 
 	public static void main(String[] args) {
